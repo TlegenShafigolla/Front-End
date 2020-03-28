f2b7c351b76c4021801f0426da468d6bf781e610
Tlegen-kz <shafigolla@icoud.com>
2020-03-28 17:47:03 +0600

    added здгы button to edit page

--- src/components/MyQuizzes/Editing/editQuiz.js
+++ src/components/MyQuizzes/Editing/editQuiz.js
@@ -2,6 +2,8 @@ import React from 'react'
 import s from './editQuizz.module.css'
 import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
+import AddIcon from "@material-ui/icons/Add";
+import IconButton from "@material-ui/core/IconButton";
 
 class editQuiz extends React.Component {
     render() {
@@ -16,7 +18,11 @@ class editQuiz extends React.Component {
                           <div className={s.questionedit}>
                               test
                           </div>
+                        <IconButton color='primary' className={s.plusebutton} >
+                            <AddIcon fontSize='large'/>
+                        </IconButton>
                     </div>
+
                     <div className={s.questions}>
                         questions
                     </div>
