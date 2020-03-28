9433a42eb096fea18b6ab54f76abbe2b55e62c83
Tlegen-kz <shafigolla@icoud.com>
2020-03-28 17:53:09 +0600

    added plus button to edit page

--- src/components/MyQuizzes/Editing/editQuiz.js
+++ src/components/MyQuizzes/Editing/editQuiz.js
@@ -2,6 +2,8 @@ import React from 'react'
 import s from './editQuizz.module.css'
 import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
+import AddIcon from "@material-ui/icons/Add";
+import IconButton from "@material-ui/core/IconButton";
 
 class editQuiz extends React.Component {
     render() {
@@ -16,6 +18,9 @@ class editQuiz extends React.Component {
                           <div className={s.questionedit}>
                               test
                           </div>
+                        <IconButton color='primary'  className={s.plusButton}>
+                            <AddIcon fontSize='large'/>
+                        </IconButton>
                     </div>
                     <div className={s.questions}>
                         questions
