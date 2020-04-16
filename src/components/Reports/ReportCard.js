803ffef667bbf3439b400798ff7698fdd2a50b2c
Tlegen-kz <shafigolla@icoud.com>
2020-04-17 02:54:08 +0600

    check box prettier

--- src/components/Reports/ReportCard.js
+++ src/components/Reports/ReportCard.js
@@ -5,39 +5,43 @@ import CardContent from "@material-ui/core/CardContent";
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 
 class ReportCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+        }
+    }
+
+
     render() {
         return (
             <div className={s.ReportCard}>
-            <CardContent className={s.CardContent}>
-
-                    <Typography gutterBottom>
-                        {"Email:" + this.props.report.email}
-                    </Typography>
-                    <Typography gutterBottom>
-                        {"To: " + this.props.report.name + " " + this.props.report.surname}
-                    </Typography>
-                    <Typography variant="body2" component="p">
-                        {"Invited: " + this.props.report.invited_date}
-                    </Typography>
-                <Typography variant="body1" component="p">
-                    {"Description: " + this.props.report.description}
+                <CardContent className={s.CardContent}>
+                    <div className={s.Person}>
+                        <Typography gutterBottom>
+                            {this.props.report.email}
+                        </Typography>
+                        <Typography gutterBottom>
+                            {this.props.report.name + " " + this.props.report.surname}
+                        </Typography>
+                        <Typography variant="body1" component="p">
+                            Scored points: {this.props.report.points ? this.props.report.total_points : this.props.report.total_corrects}
+                        </Typography>
+                    </div>
+                    <div>
+                        <Typography >
+                            {"No of questions: " + this.props.report.questions_count.toString()}
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                            Show Results: {this.props.report.showResults ? 'Yes' : 'No'}
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                            {"Invited: " + this.props.report.invited_date}
+                        </Typography>
+                    </div>
+                </CardContent>
+                <Typography className={s.version} variant="body2" color='textSecondary' component="p">
+                    {"Quiz version of: " + this.props.report.created_date}
                 </Typography>
-                <Typography color="textSecondary">
-                    {"No of questions: " + this.props.report.questions_count.toString()}
-                </Typography>
-
-
-                <Typography variant="body2" component="p">
-                    {"Mixed: " + this.props.report.mixed}
-                </Typography>
-                <Typography variant="body2" component="p">
-                    {"showResults: " + this.props.report.showResults}
-                </Typography>
-
-            </CardContent>
-            <Typography className={s.version} variant="body2" color='textSecondary' component="p">
-                {"Quiz version of: " + this.props.report.created_date}
-            </Typography>
             </div>
         );
     }
--- src/components/Reports/ReportQuestion.js
+++ src/components/Reports/ReportQuestion.js
@@ -2,20 +2,25 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import s from './ReportQuestion.module.css'
 import Checkbox from "@material-ui/core/Checkbox";
+import {green} from "@material-ui/core/colors";
+import red from "@material-ui/core/colors/red";
 
-class ReportQuestion extends React.Component{
+class ReportQuestion extends React.Component {
     render() {
-        if(this.props.val === null){
+        if (this.props.val === null) {
             return '';
         }
         let map = {};
         let key = this.props.points ? "points" : "correct";
-        for(let i = 0; i < this.props.val.session.length; i++) {
+        for (let i = 0; i < this.props.val.session.length; i++) {
             map[this.props.val.session[i].answer_id] = this.props.val.session[i][key];
         }
-        return(
+        const correct = green.A700;
+        const wrong = red.A700;
+        console.log(this.props.val)
+        return (
             <div>
-                    <div className={s.question} id={this.props.val.question_id.toString()}>
+                <div className={s.question} id={this.props.val.question_id.toString()}>
                     <div className={s.questioninfo}>
                         <div className={s.questionOrder}>{this.props.val.order_id}.</div>
                         <div className={s.questionField}>
@@ -24,24 +29,32 @@ class ReportQuestion extends React.Component{
                             </Typography>
                         </div>
                     </div>
-                    <div className={s.answer}>
+                    <div >
                         {this.props.val.answers === null ? '' : this.props.val.answers.map(val =>
-                            <div key={val.id + 'div'} className={s.answerForm}>
+                            <div key={val.id + 'div'} className={s.answer}>
+                                <div className={s.answerForm}>
                                 <Checkbox
+                                    disableRipple
+                                    style={map[val.id] > 0 ? {color: correct} : map[val.id] === 0 ? {color: wrong} : {color: 'primary'}}
                                     key={val.id + 'Checkbox'}
-                                    color='primary'
-                                    checked={val.correct > 0 || val.points > 0}
-                                    />
+                                    checked={val.point > 0 || val.correct > 0 || map[val.id] > 0 ? true : map[val.id] === 0 ? true : false}
+                                />
                                 <Typography
-                                    className={map[val.id] > 0 ? s.typographyCorrect: map[val.id] === 0 ? s.typographyWrong: s.typography}
                                     variant="body1"
                                     key={val.id}>
                                     {val.answer}
                                 </Typography>
+                                </div>
+                                <div>
+                                    {this.props.points ? (val.points > 0 ? <Typography
+                                        color='textSecondary'>points: {val.points}</Typography> : '') : (val.correct === 1 ?
+                                        <Typography color='textSecondary'>correct</Typography> : '')}
+                                    {console.log(map[val.id])}
+                                </div>
                             </div>
                         )}
                     </div>
-                    </div>
+                </div>
                 {this.props.val.type !== "FILL THE BLANK" ? null :
                     <div className={s.question}>
                         <p>Answers: </p>
