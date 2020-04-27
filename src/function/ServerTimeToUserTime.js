4c584bdc900e076a7ae9d78d57b449d744ff60da
Tlegen-kz <shafigolla@icoud.com>
2020-04-27 16:20:05 +0600

    Time isos string

--- src/function/ServerTimeToUserTime.js
+++ src/function/ServerTimeToUserTime.js
@@ -6,9 +6,6 @@ export function ServerTimeToUserTime(dates) {
     return dates;
 }
 export function UserTimeToServerTime(date) {
-    let time=new Date().getTimezoneOffset()/60
-    let s=date.getHours()+time;
-    let t=date.setHours(s);
     let dateTimeFormat=date.toISOString().replace('T',' ').replace('Z',' ')
     console.log( dateTimeFormat)
     return dateTimeFormat;
