aa459897088df6e8b4368bfd5dfc4827c75880c6
Tlegen-kz <shafigolla@icoud.com>
2020-05-17 04:00:45 +0600

    Survey starting page css adapted

--- src/pages/survey.js
+++ src/pages/survey.js
@@ -3,17 +3,21 @@ import {getInvitation, postInvitation} from "../services/API/userAPI/Survey/invi
 import CircularProgress from "@material-ui/core/CircularProgress/CircularProgress";
 import s from "../css/Survey.module.css";
 import CheckEmail from "../components/Quiz/CheckEmail";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TakeSurvey from "../components/Survey/TakeSurvey";
+import Grid from "@material-ui/core/Grid";
+import logo from "../images/logoPng.png";
+import Typography from "@material-ui/core/Typography";
 
 class Survey extends React.Component{
     constructor(props) {
         super(props);
         const path = window.location.pathname.split('/');
         const session_id = localStorage.getItem(`session_id${path[2]}`);
+        const survey = localStorage.getItem(`survey_session${path[2]}`) ? JSON.parse(localStorage.getItem(`survey_session${path[2]}`)) : null;
         this.state = {
             link: path[2],
+            survey:survey,
             correctLink: session_id !== null ? true : null,
             error: null,
             correctEmail: session_id !== null ? true : null,
@@ -34,7 +38,8 @@ class Survey extends React.Component{
             this.setState({statusEmail: 'Success' === json.Status});
             if (json.Status === 'Success') {
                 localStorage.setItem(`session_id${this.state.link}`, json['session_id']);
-                window.location.reload();
+                localStorage.setItem(`survey_session${this.state.link}`, JSON.stringify(json.survey));
+                this.setState({survey:json.survey})
             }
             if (json.Status === 'Failed') {
                 this.setState({emailError: true})
@@ -65,7 +70,7 @@ class Survey extends React.Component{
                 onChangeEmail={this.onChangeEmail}
                 onClickContinue={this.onClickContinue}/>
         } else if(this.state.session_id && !this.state.survey_started){
-            return(<StartDialog start={this.onStartClicked}/>);
+            return(<StartDialog survey={this.state.survey} start={this.onStartClicked}/>);
         } else if(this.state.session_id && this.state.survey_started){
             return(<TakeSurvey />)
         }
@@ -94,7 +99,23 @@ const Error = (props) => {
 const StartDialog = (props) => {
     return (
         <div>
+            <Grid container
+                  spacing={3}
+                  direction="column"
+                  justify="space-between"
+                  alignItems="center">
+                <img className={s.Logo} src={logo} alt="Logo"/>
+                <span className={s.QuizName}>{props.survey.survey_name}</span>
+                <span className={s.SurveyCount}>{props.survey.questions_count} Questions</span>
+                <div className={s.Instructions}>
+                    <Typography paragraph color="primary" variant="h6">{"Description"}</Typography>
+                    <div className={s.Description}>
+                        <Typography color="textSecondary" variant="body1">{props.survey.description}</Typography>
+                    </div>
+
+                </div>
             <Button color='primary' onClick={props.start} variant='contained'>Start Survey</Button>
+            </Grid>
         </div>
     )
 };
