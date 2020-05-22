c75aa6805d2477c7d09a571e7bbbd1317edf05d7
askargabit <askargabitm@gmail.com>
2020-05-23 00:47:13 +0600

    Added GET methods for Group Quizzes and Surveys

--- src/components/Groups/EditGroup.js
+++ src/components/Groups/EditGroup.js
@@ -7,32 +7,35 @@ import Members from "./Members";
 import s from './EditGroup.module.css'
 import TextField from "@material-ui/core/TextField";
 import {putGroupName} from "../../services/API/adminAPI/Group/group";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import {getGroupQuizzes} from "../../services/API/adminAPI/Group/quizzes";
+import {getGroupSurveys} from "../../services/API/adminAPI/Group/surveys";
 
 class EditGroup extends React.Component {
     constructor(props) {
         super(props);
         const path = window.location.pathname.split('/');
         this.state = {
-            path: path[3],
+            group_id: path[3],
             members: [],
             editGroupName: false,
             group_name: null,
             email: null,
             addMember: false,
-            group_name_change: false
+            group_name_change: false,
+            surveys: null,
+            quizzes: null
         }
     }
 
     componentDidMount() {
-        const path = window.location.pathname.split('/');
-        getMembers(path[3]).then(json => this.setState({
+        getMembers(this.state.group_id).then(json => this.setState({
             members: json.members,
             group_name: json.group_name,
-        }))
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-
+        }));
+        getGroupQuizzes(this.state.group_id).then(json => this.setState({quizzes: json.invitations}));
+        getGroupSurveys(this.state.group_id).then(json => this.setState({surveys: json.invitations}));
     }
 
     addMembers = (e) => {
@@ -40,7 +43,7 @@ class EditGroup extends React.Component {
         let email = /[0-9a-z_-]+@[0-9a-z_-]+\.[a-z]{2,5}$/i;
         if (email.test(this.state.email)) {
             let members = this.state.members;
-            addMembers(this.state.path, this.state.email).then(json => {
+            addMembers(this.state.group_id, this.state.email).then(json => {
                     if (json.Error === undefined) {
                         members.push(json);
                         this.setState({
@@ -56,28 +59,27 @@ class EditGroup extends React.Component {
                 addMember: false
             })
         }
-    }
+    };
     onBlurGroupName = () => {
         if (this.state.group_name !== '') {
             this.setState({editGroupName: false});
             if (this.state.group_name_change) {
-                putGroupName(this.state.path, this.state.group_name).then(() =>
+                putGroupName(this.state.group_id, this.state.group_name).then(() =>
                     this.setState({group_name_change: false}))
             }
 
         }
-    }
+    };
     onChangeGroupName = (e) => {
-        let group_name = this.state.group_name
+        let group_name = this.state.group_name;
         if (group_name !== e.target.value.trim()) {
             this.setState({group_name_change: true})
         }
         this.setState({group_name: e.target.value.trim()})
-    }
+    };
     onDeleteMember = (id) => {
         deleteMember(id, this.state.path).then(json => {
-            console.log(json)
-            let members = this.state.members
+            let members = this.state.members;
             for (let i = 0; i < members.length; i++) {
                 if (members[i]._id === json._id) {
                     members.splice(i, 1)
@@ -85,36 +87,56 @@ class EditGroup extends React.Component {
                 this.setState({members: members})
             }
         })
-    }
+    };
     onChangeEmail = e => {
         this.setState({email: e.target.value.trim()})
-    }
+    };
 
     render() {
         return (
-            <div className={s.EditGroup}>
-                <div className={s.GroupName} onClick={() => this.setState({editGroupName: true})}>
-                    {!this.state.editGroupName ? <Typography>{this.state.group_name}</Typography>
-                        : <TextField autoFocus={true} onBlur={this.onBlurGroupName}
-                                     onChange={this.onChangeGroupName} defaultValue={this.state.group_name}/>}
-                </div>
-                <div className={s.Quiz}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi harum nulla rerum velit. A
-                    accusantium alias facere officia praesentium. Quia.
-                </div>
-                <div className={s.MembersBoard}>
-                    <ol type='1'>{this.state.members.map(val => <li key={val._id}><Members path={this.state.path}
-                                                                                           onDeleteMember={this.onDeleteMember}
-                                                                                           val={val}/></li>)}</ol>
-                    {this.state.addMember ?
-                        <form onSubmit={this.addMembers}>
-                            <TextField autoFocus={true} onBlur={this.addMembers} onChange={this.onChangeEmail}/>
-                        </form> :
-                        <IconButton color="primary" size='small' onClick={() => this.setState({addMember: true})}>
-                            <AddIcon fontSize='small'/>
-                        </IconButton>}
-                </div>
-            </div>
+            <Grid
+                container
+                justify="flex-end"
+                alignItems="flex-start"
+                spacing={1}>
+                <Grid item lg={6} md={8} sm={10} xs={12}>
+                    <Paper square elevation={3} className={s.GroupName}>
+                        <div onClick={() => this.setState({editGroupName: true})}>
+                            {!this.state.editGroupName ? <Typography>{this.state.group_name}</Typography>
+                                : <TextField autoFocus={true} onBlur={this.onBlurGroupName}
+                                             onChange={this.onChangeGroupName} defaultValue={this.state.group_name}/>}
+                        </div>
+                    </Paper>
+                    <Grid container spacing={1}>
+                        <Grid item lg={6} md={6} sm={12} xs={12}>
+                            <Paper square elevation={3} className={s.QuizList}>
+                                Quiz Quiz
+                            </Paper>
+                        </Grid>
+                        <Grid item lg={6} md={6} sm={12} xs={12}>
+                            <Paper square elevation={3} className={s.SurveyList}>
+                                Survey Survey
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item lg={3} md={2} sm={10} xs={12}>
+                    <Paper square elevation={3} className={s.MembersBoard}>
+                    <div>
+                        <ol type='1'>{this.state.members.map(val => <li key={val._id}><Members path={this.state.path}
+                                                                                               onDeleteMember={this.onDeleteMember}
+                                                                                               val={val}/></li>)}</ol>
+                        {this.state.addMember ?
+                            <form onSubmit={this.addMembers}>
+                                <TextField autoFocus={true} onBlur={this.addMembers} onChange={this.onChangeEmail}/>
+                            </form> :
+                            <IconButton color="primary" size='small' onClick={() => this.setState({addMember: true})}>
+                                <AddIcon fontSize='small'/>
+                            </IconButton>}
+                    </div>
+                    </Paper>
+                </Grid>
+            </Grid>
         )
     }
 }
