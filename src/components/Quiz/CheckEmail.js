12903de7a3c30b1529cec7a73380df8b52805312
Tlegen-kz <shafigolla@icoud.com>
2020-07-09 16:49:19 +0600

    change default port

--- src/components/Quiz/CheckEmail.js
+++ src/components/Quiz/CheckEmail.js
@@ -4,28 +4,27 @@ import DialogContent from "@material-ui/core/DialogContent";
 import TextField from "@material-ui/core/TextField";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
- class CheckEmail  extends React.Component{
-     render() {
-         localStorage.removeItem('start_test')
-         return (
-             <div>
-                 <Dialog open={true} fullWidth={true} maxWidth='xs'>
-                     <DialogContent>
-                         <TextField
-                                    error={this.props.error}
-                                    margin="dense"
-                                    id="Email"
-                                    label="Email Address"
-                                    type="email"
-                                    onChange={this.props.onChangeEmail}
-                                    fullWidth/>
-                     </DialogContent>
-                     <DialogActions>
-                         <Button onClick={this.props.onClickContinue}>Continue</Button>
-                     </DialogActions>
-                 </Dialog>
-             </div>
-         );
-     }
- }
- export default CheckEmail;
+ const CheckEmail=(props) =>{
+    localStorage.removeItem('start_test')
+    return (
+        <div>
+            <Dialog open={true} fullWidth={true} maxWidth='xs'>
+                <DialogContent>
+                    <TextField
+                        error={props.error}
+                        margin="dense"
+                        id="Email"
+                        label="Email Address"
+                        type="email"
+                        onChange={props.onChangeEmail}
+                        fullWidth/>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={props.onClickContinue}>Continue</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default CheckEmail;
